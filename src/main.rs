ea00c4e4b325885662a277ec99095e67750c0203
Zayaan-taha <zayaan.taha@icloud.com>
2024-02-29 21:58:42 +0530

    The application can now generate TOTPs Renamed Token to Account Account now takes the fields time, digits and skew Implemented From<Account> for Algorithm Moved generate_token to token.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use token::Token;
+use token::Account;
+
+use crate::token::generate_token;
 
 pub mod token;
 
 fn main() {
     let path = std::env::args().nth(1).unwrap();
-    let token = Token::load_token(path).unwrap();
-    println!("{token:?}");
+    let account = Account::load_token(path).unwrap();
+    let token = generate_token(account).unwrap();
+    println!("{:?}", token);
 }
--- src/token.rs
+++ src/token.rs
@@ -1,5 +1,6 @@
 use eyre::Result;
 use serde::Deserialize;
+use totp_rs::{Algorithm, Secret, TOTP};
 
 #[derive(Debug, Deserialize)]
 pub enum EncryptionAlgo {
@@ -8,16 +9,43 @@ pub enum EncryptionAlgo {
     SHA512,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Token {
-    pub token: String,
-    pub algorithm: EncryptionAlgo,
+impl From<EncryptionAlgo> for Algorithm {
+    fn from(value: EncryptionAlgo) -> Self {
+        match value {
+            EncryptionAlgo::SHA1 => Algorithm::SHA1,
+            EncryptionAlgo::SHA256 => Algorithm::SHA256,
+            EncryptionAlgo::SHA512 => Algorithm::SHA512,
+        }
+    }
 }
 
-impl Token {
-    pub fn load_token(path: String) -> Result<Token> {
+#[derive(Debug, Deserialize)]
+pub struct Account {
+    pub secret: String,
+    pub time: u64,
+    pub algorithm: EncryptionAlgo,
+    pub digits: Option<usize>,
+    pub skew: Option<u8>,
+}
+
+impl Account {
+    pub fn load_token(path: String) -> Result<Account> {
         let file = std::fs::read_to_string(path)?;
-        let token: Token = serde_json::from_str(&file)?;
-        return Ok(token);
+        let account: Account = serde_json::from_str(&file)?;
+        return Ok(account);
     }
+}
+
+pub fn generate_token(account: Account) -> Result<String> {
+    let secret = Secret::Encoded(account.secret).to_string();
+    let totp = TOTP::new(
+        account.algorithm.into(),
+        account.digits.unwrap_or(6),
+        account.skew.unwrap_or(1),
+        account.time,
+        secret.into_bytes(),
+    )
+    .unwrap();
+    let token = totp.generate_current()?;
+    Ok(token)
 }
